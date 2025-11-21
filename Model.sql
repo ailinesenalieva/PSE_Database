0fe822a4e1df6a7c055c321d718ed33486c65c8a
lenjaKrah <s_krah23@stud.hwr-berlin.de>
2025-11-21 14:38:21 +0100

    Syntax error korrigieren

--- Model.sql
+++ Model.sql
@@ -3,12 +3,12 @@ CREATE TABLE points (
    point_ID SERIAL PRIMARY KEY, 
    name VARCHAR(100) NOT NULL UNIQUE,
    x DOUBLE PRECISION NOT NULL,  -- X-Koordinate des Knotens
-   y DOUBLE PRECISION NOT NULL,  -- Y-Koordinate des Knotens
+   y DOUBLE PRECISION NOT NULL  -- Y-Koordinate des Knotens
 ); 
 
 -- Tabelle für Points of Interest (POIs)
 CREATE TABLE poi (
-    point_ID INTEGER PRIMARY KEY REFERENCES points(point_ID),
+    poi_ID INTEGER PRIMARY KEY REFERENCES points(point_ID),
     descriptionGER TEXT,
     descriptionENG TEXT
 );
